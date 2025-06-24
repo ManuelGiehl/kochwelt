448031fd1a7867586e2d22639d42bf90be52d259
Manuel <manuzcore1@gmail.com>
2025-06-24 03:13:12 +0200

    shortend css, added better responsive and mail confirmation as well as reset for the contactform

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+// Burger Menu ein und ausblenden
+function toggleMenu() {
+    var menu = document.getElementById("menuButton");
+    menu.classList.toggle("d-none");
+}
+
+// Kontaktformular zurücksetzen
+function resetContactForm() {
+    var contactForm = document.querySelector('.contact-form');
+    if (contactForm) {
+        contactForm.reset();
+    }
+}
+
+// Event-Listener für das Kontaktformular
+document.addEventListener('DOMContentLoaded', resetContactForm);
+window.addEventListener('pageshow', resetContactForm);
+
 document.addEventListener('DOMContentLoaded', function() {
     var btn = document.getElementById('cheesecakeBtn');
     if (btn) {
         btn.addEventListener('click', function() {
             window.location.href = 'cheesecake.html';
         });
-    }
-
-    var portionInput = document.getElementById('portionInput');
-    var portionBtn = document.getElementById('portionBtn');
-    var ingredientsList = document.querySelectorAll('.cheesecake-ingredients-list li');
-    var defaultPortion = 4;
-
-    
-
-    if (portionBtn && portionInput && ingredientsList.length) {
-        portionBtn.addEventListener('click', updateIngredients);
     }
 });
